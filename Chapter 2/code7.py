1bc7b3a7e217a95fead2185a328faf10deeac7f2
Vishakh <101414781+Vishakh25@users.noreply.github.com>
2025-05-14 11:20:51 +0530

    Add files via upload

---
+++ Chapter 2/code7.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter your marks (0 to 100): "))
+
+if marks >= 90:
+    grade = 'A'
+elif marks >= 80:
+    grade = 'B'
+elif marks >= 70:
+    grade = 'C'
+elif marks >= 60:
+    grade = 'D'
+else:
+    grade = 'E'
+print("Your grade is: ", grade)
