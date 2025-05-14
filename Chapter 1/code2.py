8d1b836ab01743468f2cee8f479b95fbe80b42e5
Vishakh <101414781+Vishakh25@users.noreply.github.com>
2025-05-14 11:19:42 +0530

    Add files via upload

---
+++ Chapter 1/code2.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("enter the second number: "))
+
+s = num1+num2
+print(s)
---
+++ Chapter 1/code3.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("Swaped elements are : ")
+print(a)
+print(b)
